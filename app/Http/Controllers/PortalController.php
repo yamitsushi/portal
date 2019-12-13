8324821eae8d077f31ab396c3786b45eb8a2e9fb
yamitsushi <yamitsushi@gmail.com>
2019-12-13 13:43:09 +0000

    record mac in tag

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
 
 class PortalController extends Controller
 {
     public function tag(Request $request)
     {
-        
-        return response('', 204);
+        $arp= 'arp -a ' . $request->ip();
+        $lines=explode(" ", exec($arp));
+        return response()->json(Client::firstOrCreate(['mac' => $lines[3]]));
     }
 }
