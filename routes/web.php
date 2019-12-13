db00af351f063889bf4c49c2b4499212a45bab1f
yamitsushi <yamitsushi@gmail.com>
2019-12-13 12:25:37 +0000

    add client database management

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,23 @@
 */
 
 Route::prefix('/')->group(function () {
-  Route::post('login', function () {return response('', 204);});
-  Route::get('mac', function () {dd(shell_exec('getmac'));});
+  Route::post('tag', 'PortalController@tag');
+  //Route::post('login', function () {return response('', 204);});
+  Route::get('mac', function () {
+    $ipAddress= $_SERVER['REMOTE_ADDR'];
+    $arp= 'arp -a ' . $ipAddress;
+    $lines=explode(" ", exec($arp));
+    dd($lines[3]);
+  });
+  Route::get('check', function() {
+    $response = $pusher->get( '/channels/pulse/users' );
+if( $response[ 'status'] == 200 ) {
+  // convert to associative array for easier consumption
+  $users = json_decode( $response[ 'body' ], true )[ 'users' ];
+}
+
+dd(count($users));
+  });
 });
 
 Route::view('{any1?}/{any2?}/{any3?}', 'pages/spa');
